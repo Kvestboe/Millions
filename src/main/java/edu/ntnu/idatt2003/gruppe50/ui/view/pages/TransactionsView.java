6ec8832ce2788375f38e34a2847720d2ce641da6
Kristian <vestboe.kristian@gmail.com>
2026-05-09 15:08:05 +0200

    feat(view): add TransactionsView

---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/ui/view/pages/TransactionsView.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idatt2003.gruppe50.ui.view.pages;
+
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+public class TransactionsView extends BorderPane implements Page {
+
+  @Override
+  public Parent getView() {
+    return this;
+  }
+
+}
