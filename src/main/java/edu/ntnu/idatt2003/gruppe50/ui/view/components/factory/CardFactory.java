b3a49a37410c5dd96eb2de42eed71263e89b12d8
Kristian <vestboe.kristian@gmail.com>
2026-05-11 17:21:52 +0200

    feat(ui): refactor factory classes to factory package, add table factory

--- src/main/java/edu/ntnu/idatt2003/gruppe50/ui/view/components/factory/CardFactory.java
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/ui/view/components/factory/CardFactory.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idatt2003.gruppe50.ui.view.components;
+package edu.ntnu.idatt2003.gruppe50.ui.view.components.factory;
 
 import javafx.scene.Node;
 import javafx.scene.control.Label;
---
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/ui/view/components/factory/TableFactory.java
@@ -0,0 +1,31 @@
+package edu.ntnu.idatt2003.gruppe50.ui.view.components.factory;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.List;
+
+public final class TableFactory {
+
+  private TableFactory() {}
+
+  public static <T> TableView<T> createTable(
+      List<ColumnDefinition<T, ?>> definitionList
+  ) {
+    TableView<T> table = new TableView<>();
+
+    for (ColumnDefinition<T, ?> definition : definitionList) {
+      table.getColumns().add(createColumn(definition));
+    }
+
+    table.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+    return table;
+  }
+
+  private static <T, V> TableColumn<T, V> createColumn(ColumnDefinition<T, V> definition) {
+    TableColumn<T, V> column = new TableColumn<>(definition.title());
+    column.setCellValueFactory(cell -> definition.getter().apply(cell.getValue()));
+    return column;
+  }
+
+}
