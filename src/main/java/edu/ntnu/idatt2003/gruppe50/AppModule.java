190f52674517bb842d429dc41411ea2e3e661bcc
Marius Klepp <Mariusklepp@icloud.com>
2026-05-26 15:09:37 +0200

    feat(application): add WeeklyMoversDto and GetWeeklyMoversUseCase

--- src/main/java/edu/ntnu/idatt2003/gruppe50/AppModule.java
+++ src/main/java/edu/ntnu/idatt2003/gruppe50/AppModule.java
@@ -22,6 +22,7 @@ import edu.ntnu.idatt2003.gruppe50.application.query.GetStockDetailUseCase;
 import edu.ntnu.idatt2003.gruppe50.application.query.GetTradingLogUseCase;
 import edu.ntnu.idatt2003.gruppe50.application.query.GetTransactionMarkersUseCase;
 import edu.ntnu.idatt2003.gruppe50.application.query.GetTransactionsUseCase;
+import edu.ntnu.idatt2003.gruppe50.application.query.GetWeeklyMoversUseCase;
 import edu.ntnu.idatt2003.gruppe50.application.query.PreviewOrderUseCase;
 import edu.ntnu.idatt2003.gruppe50.domain.game.GameSession;
 import edu.ntnu.idatt2003.gruppe50.domain.leaderboard.Leaderboard;
@@ -65,6 +66,7 @@ public final class AppModule {
   public final PurchaseShopItemUseCase purchaseShopItem = new PurchaseShopItemUseCase(sessions);
   public final GetGoalProgressUseCase getGoalProgress = new GetGoalProgressUseCase(sessions);
   public final GetStatusProgressUseCase getStatusProgress = new GetStatusProgressUseCase(sessions);
+  public final GetWeeklyMoversUseCase getWeeklyMovers = new GetWeeklyMoversUseCase(sessions);
 
   public GameSessionControllerBundle gameBundle(UUID gameId) {
     GameSession session = sessions.findById(gameId).orElseThrow(GameSessionNotFoundException::new);
